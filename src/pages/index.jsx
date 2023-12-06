3c5c5af09f629c3d9eb62bc5eb9008bb3e3df655
Aaron McGuire <aaron@gothicginobili.com>
2023-12-06 10:39:38 -0500

    last few changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ const IndexPage = ({data}) => {
       <>
         <Warning emoji="">Everything on this site is a potential KOL spoiler. Proceed at your own risk!</Warning>
 
-        <p>Welcome to <b>Loathers.net</b>, a new site managed by the <a href="https://github.com/loathers/">Loathers Scripting Collective</a>! We are a group of coders who play and build tools for the now-20-year-old browser game <a href="https://www.kingdomofloathing.com/">the Kingdom of Loathing.</a> We're going to be using this site over the coming years to store analysis of the modern speed game, how our collective scripts the game, and host semi-annual contests. We hope you enjoy our dulcet tones and sick beats.</p>
+        <p>Welcome to <b>Loathers.net</b>, a new site managed by the <a href="https://github.com/loathers/">Loathers Scripting Collective</a>! We are a group of coders who play and contribute to a long-running browser game, <a href="https://www.kingdomofloathing.com/">the Kingdom of Loathing.</a> We're going to be using this site over the coming years to store analysis of the modern speed game, how our collective scripts tools for the game, and host semi-annual contests. We hope you enjoy our dulcet tones and sick beats.</p>
 
         {data.allMdx.nodes.map(
           ( {excerpt, frontmatter }) => (
